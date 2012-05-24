baf29221fc6ea2187fb60b51c868a37efae6fab6
Darius Jazayeri <jazayeri@alum.mit.edu>
2012-05-24 11:13:05 -0700

    Followup refactoring on UIFR-48

--- omod/src/main/webapp/resources/scripts/ui.js
+++ omod/src/main/webapp/resources/scripts/ui.js
@@ -1,48 +1,64 @@
 // we will migrate functionality from uiframework.js into here, refactoring it as we go 
 var ui = (function($) {
 
+	var setupAjaxPostDefaults = {
+		dataType: "json",
+		globalErrorContainer: ".global-error-container",
+		globalErrorContent: ".global-error-content",
+		onError: function(xhr, form, globalError) {
+			try {
+				var err = $.parseJSON(xhr.responseText);
+				for (var i = 0; i < err.globalErrors.length; ++i) {
+					globalError.append('<li>' + err.globalErrors[i] + '</li>');
+				}
+				for (key in err.fieldErrors) {
+					var errorMsg =  err.fieldErrors[key].join(', ');
+					var errorField = form.find('input[name="' + key + '"]').nextAll('.error');
+					if (errorField.length == 0) {
+						globalError.append('<li>' + errorMsg + '</li>');
+					} else {
+						errorField.append(errorMsg + '<br/>').show();
+					}
+				}
+			} catch (ex) {
+				notifyError("Failed " + ex + " (" + xhr.responseText + ")");
+			}
+		}
+	}
+	
 	return {
 		
 		/*
 		 * Takes an existing <form> element and sets it up to submit via AJAX and get a json response.
 		 * 
-		 * onSuccess is required, and should should be a one-arg function that called with a parsed json object
-		 * 
-		 * onError is optional and should be a one-arg function that is called with a jqXHR object.
-		 * if unspecified, the default behavior is to try to show field errors in .error and .global-error elements.
+		 * options:
+		 * - onSuccess (required) should should be a one-arg function that called with a parsed json object
+		 * - onError (optional) should a function(jqXHR, form, globalErrorContent). Defaults to trying to show field errors in .error and globalErrorContent elements.
+		 * - dataType (default "json") should be a string representing the datatype of the returned data
+		 * - globalErrorContainer (default ".global-error-container")
+		 * - globalErrorContent (default ".global-error-content")
 		 */
-		setupJsonPost: function(formSelector, onSuccess, onError) {
+		setupAjaxPost: function(formSelector, options) {
+			if (typeof options.onSuccess !== 'function') {
+				throw "onSuccess is required";
+			}
+			
+			var opts = $.extend({}, setupAjaxPostDefaults, options);
+			
 			$(formSelector).submit(function(event) {
 				event.preventDefault();
 				var form = $(this);
 
 				// find error fields (and clear them)
-				var globalError = form.find('.global-error').html('').hide();
+				var globalError = form.find(opts.globalErrorContent).html('');
+				var globalErrorContainer = form.find(opts.globalErrorContainer).hide();
 				form.find('.error').html('').hide();
 				
-				if (typeof onError !== 'function') {
-					onError = function(xhr) {
-						try {
-							var err = $.parseJSON(xhr.responseText);
-							for (var i = 0; i < err.globalErrors.length; ++i) {
-								globalError.append(err.globalErrors[i] + '<br/>').show();
-							}
-							for (key in err.fieldErrors) {
-								var errorMsg =  err.fieldErrors[key].join(', ');
-								var errorField = form.find('input[name="' + key + '"]').nextAll('.error');
-								if (errorField.length == 0) {
-									errorField = globalError;
-								}
-								errorField.append(errorMsg + '<br/>').show();
-							}
-						} catch (ex) {
-							notifyError("Failed " + xhr.responseText);
-						}
-					}
-				}
-				
 				// POST and get back the result as JSON
-				$.post(form.attr('action'), form.serialize(), onSuccess, "json").error(onError);
+				$.post(form.attr('action'), form.serialize(), opts.onSuccess, opts.dataType).error(function(xhr) {
+					globalErrorContainer.show();
+					opts.onError(xhr, form, globalError);
+				});
 			});
 		}
 	
